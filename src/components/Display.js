4bfa1dca10e5ab6945829d2a2ed56c667d7790ba
Tomas Larrauri <tomas.larrauri@melon.tech>
2018-09-01 13:02:39 -0300

    add funcionalidad: mostrar videos y share on tw

--- src/components/Display.js
+++ src/components/Display.js
@@ -3,13 +3,31 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import Nav from './Nav';
+import { Share } from 'react-twitter-widgets';
 import { CloudinaryContext, Transformation, Video } from 'cloudinary-react';
 import { isLoggedIn } from '../utils/AuthService';
 import axios from 'axios';
 
+
 class Display extends Component {
 
+  state = { videos: [] };
+
+  getVideos() {
+    axios.get('http://res.cloudinary.com/unicodeveloper/video/list/miniflix.json')
+          .then(res => {
+            console.log(res.data.resources);
+            this.setState({ videos: res.data.resources});
+    });
+  }
+
+  componentDidMount() {
+    this.getVideos();
+  }
+
   render() {
+
+  	const { videos }  = this.state;
 
     return (
       <div>
@@ -18,6 +36,20 @@ class Display extends Component {
         <hr/>
 
         <div className="col-sm-12">
+        	<CloudinaryContext cloudName="unicodeveloper">
+            { videos.map((data, index) => (
+                <div className="col-sm-4" key={index}>
+                  <div className="embed-responsive embed-responsive-4by3">
+                    <Video publicId={data.public_id} width="300" height="300" controls></Video>
+                  </div>
+                  <div className="tweet-share text-center">
+                	<Share url={`http://res.cloudinary.com/unicodeveloper/video/upload/${data.public_id}.mp4`} /> 
+                  </div> 
+                </div>
+
+              ))
+            }
+          </CloudinaryContext>
 
         </div>
       </div>
